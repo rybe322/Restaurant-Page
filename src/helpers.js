ce1083406d47bafccd87ba863852c58afa23313f
Ryan Bennett <Ryan.Bennett322@gmail.com>
2022-10-06 15:50:29 -0500

    Update home and add hours

--- src/helpers.js
+++ src/helpers.js
@@ -5,3 +5,21 @@ export const createHTMLElement = (elementType, attrs) => {
   }
   return element
 }
+
+export const elFactory = (type, attributes, children) => {
+  const el = document.createElement('type')
+
+  for (let key in attributes) {
+    el.setAttribute(key, attributes[key])
+  }
+
+  children.forEach(child => {
+    if (typeof child === 'string') {
+      el.appendChild(document.createTextNode(child))
+    } else {
+      el.appendChild(child)
+    }
+  })
+
+  return el
+}
--- src/home.js
+++ src/home.js
@@ -13,6 +13,8 @@ Parallel 37
 Eleven Madison Park
 
 
+Use time element for time-related piece of information.
+
 
 <div id="root">
   <div id="content">
@@ -27,14 +29,62 @@ Eleven Madison Park
   </div>
 </div>
 
+
+ <p><meta itemprop="opens" content="08:00:00">8:00 a.m. -<meta itemprop="closes" content="20:00:00">8:00 p.m.</p>
+  
+
 */
 
 
 export const Home = () => {
-  const contentDiv = document.createElement('div')
+  const contentDiv = createHTMLElement('div', {id: 'content'})
+
+  const getAbout = () => {
+    const aboutDiv = createHTMLElement('div', {id:'about-div'})
+    const aboutH2 = createHTMLElement('h2', {textContent: 'About'})
+    const aboutP = createHTMLElement('p', {textContent: 'About section'})
+    aboutDiv.appendChild(aboutH2)
+    aboutDiv.appendChild(aboutP)
+    return aboutDiv
+  }
+
+  const makeHoursElement = (day, openingHours, closingHours) => {
+    const textContent = `${day}: ${openingHours} A.M - ${closingHours} P.M`
+    return createHTMLElement('p', {
+      class: 'hours-row',
+      'textContent': textContent
+    })
+  }
+
+  const getHours = () => {
+    const hoursDiv = createHTMLElement('div', {id: 'hours-div'})
+    const hoursH2 = createHTMLElement('h2', {textContent: 'Hours'})
+
+    hoursDiv.appendChild(hoursH2)
+    hoursDiv.appendChild(makeHoursElement('Sunday', '08:00', '09:00'))
+    hoursDiv.appendChild(makeHoursElement('Monday', '08:00', '09:00'))
+    hoursDiv.appendChild(makeHoursElement('Tuesday', '08:00', '09:00'))
+    hoursDiv.appendChild(makeHoursElement('Wednesday', '08:00', '09:00'))
+    hoursDiv.appendChild(makeHoursElement('Thursday', '08:00', '09:00'))
+    hoursDiv.appendChild(makeHoursElement('Friday', '08:00', '09:00'))
+    hoursDiv.appendChild(makeHoursElement('Saturday', '08:00', '09:00'))
+    return hoursDiv
+  }
+
+  const getLocation = () => {
+    const locationDiv = createHTMLElement('div', {id: 'location-div'})
+    const locationH2 = createHTMLElement('h2', {textContent: 'Location'})
+    const address = createHTMLElement('address', {textContent: '123 Fake St. Fake Illinois 12345'})
+    locationDiv.appendChild(locationH2)
+    locationDiv.appendChild(address)
+    return locationDiv
+  }
 
   const getHome = () => {
-    return createHTMLElement('div',{textContent: 'Home'})
+    contentDiv.appendChild(getAbout())
+    contentDiv.appendChild(getHours())
+    contentDiv.appendChild(getLocation())
+    return contentDiv
   }
 
   return {getHome}
