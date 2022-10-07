160d9783c330ca13ab1c405485ff710a08bbfafe
Ryan Bennett <Ryan.Bennett322@gmail.com>
2022-10-07 15:00:32 -0500

    Finish adding and styling contact page

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,28 @@
 import { createHTMLElement } from "./helpers"
 
 export const Contact = () => {
+  const contentDiv = createHTMLElement('div', {id: 'content'})
+
+  const createContactRow = (contactType, contactText) => {
+    const contactRowDiv = createHTMLElement('div')
+    contactRowDiv.classList.add('contact-div')
+    const contactH2 = createHTMLElement('h2', {textContent: contactType})
+    const contactP = createHTMLElement('p', {textContent: contactText})
+    contactRowDiv.appendChild(contactH2)
+    contactRowDiv.appendChild(contactP)
+    return contactRowDiv
+
+  }
+
 
   const getContact = () => {
-    return createHTMLElement('div',{textContent: 'CONTACT'})
+    contentDiv.appendChild(createHTMLElement('h1',{id: 'contact-header' ,textContent: 'Contact us' }     ))
+    contentDiv.appendChild(createContactRow('Phone','123-456-7890'))
+    contentDiv.appendChild(createContactRow('Email','contact@parallel37.com'))
+    contentDiv.appendChild(createContactRow('Socials', 'Twitter | Instagram | Facebook | Others'))
+    
+
+    return contentDiv
   }
 
   return {getContact}
